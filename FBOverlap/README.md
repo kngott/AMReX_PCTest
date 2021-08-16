bbf15e24305ef7dd9bd05fc3af7930848c2583c5
Kevin Gott <kngott@lbl.gov>
2021-08-16 01:35:24 -0700

    FBOverlap

---
+++ FBOverlap/README.md
@@ -0,0 +1,7 @@
+# Masking 
+
+
+Simple Masking Test
+Comparing times for various size and shapes of Multifabs. Compares time to complete "interior" versus masked "boundary" regions.
+
+For potential use in overlap with Communication operations involving boundaries.
---
+++ FBOverlap/main.cpp
@@ -0,0 +1,267 @@
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_VisMF.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_BLProfiler.H>
+#include <AMReX_MultiFabUtil.H>
+
+using namespace amrex;
+void main_main ();
+
+AMREX_GPU_DEVICE
+void compute_bound(int i, int j, int k, int n,
+                   Array4<Real> arr, int nw)
+{
+    Real y = arr(i,j,k,n);
+    Real x = 1.0;
+    for (int n = 0; n < nw; ++n) {
+        Real dx = -(x*x-y) / (2.*x);
+        x += dx;
+    }
+    arr(i,j,k,n) = x;
+}
+
+// ================================================
+
+int main (int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+    main_main();
+
+    amrex::Finalize();
+}
+
+// ================================================
+
+void main_main ()
+{
+
+    BL_PROFILE("main");
+
+    int ncell, ncomp, nghost, mgs, nwork=20, ntest=20;
+    IntVect lo, hi, bvect;
+    {
+        ParmParse pp;
+        pp.get("lo", lo);
+        pp.get("hi", hi);
+        pp.get("ncomp", ncomp);
+        pp.get("nghost", nghost);
+        pp.get("max_grid_size", mgs);
+        pp.get("boundary", bvect);
+        pp.query("nwork", nwork);
+        pp.query("nwork_test", ntest);
+    }
+
+    MultiFab mf;
+    iMultiFab bmask;
+    Box domain(lo, hi);
+
+    BoxArray ba(domain);
+    ba.maxSize(mgs);
+    DistributionMapping dm(ba);
+
+    Real start_val = 3.14159;
+    mf.define(ba, dm, ncomp, nghost);
+    mf.setVal(start_val);
+
+    bmask.define(ba, dm, 1, 0);
+    bmask.setVal(0);
+
+    amrex::Print() << std::endl;
+    amrex::Print() << "domain = " << domain << std::endl;
+    amrex::Print() << "ncomp = " << ncomp << std::endl;
+    amrex::Print() << "ba = " << ba << std::endl;
+    amrex::Print() << "mgs = " << mgs << std::endl;
+    amrex::Print() << "nghost = " << nghost << std::endl;
+
+    // Build Mask
+    {
+        BL_PROFILE("** Mask Build");
+
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+        {
+            const Box& bx = mfi.validbox();
+            auto mfv = bmask.array(mfi);
+
+            Box ibx = bx;
+            for (int i=0; i<AMREX_SPACEDIM; ++i)
+            {
+                ibx.growLo(i, -bvect[i]);
+                ibx.growHi(i, -bvect[i]);
+            }
+
+            amrex::ParallelFor(bx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k)
+            {
+                if (!ibx.contains(i,j,k))
+                    { mfv(i,j,k) = 1; }
+            }); 
+        }
+    }
+
+    long cells = ba.numPts();
+    long bcells = bmask.sum(0)*ncomp; 
+
+    amrex::Print() << std::endl;
+    amrex::Print() << "Boundary Cells: " << bcells << std::endl; 
+    amrex::Print() << "Total Cells: " << cells << std::endl;
+    amrex::Print() << "Boundary %: " << 100*(double(bcells)/double(cells)) << std::endl;
+
+    amrex::Print() << std::endl;
+    amrex::Print() << "nwork: " << nwork << std::endl;
+    amrex::Print() << " ========================= " << std::endl;
+
+    double t_all, t_bound, t_int;
+
+// =======================================================
+
+    for (int nw=1; nw<=nwork; ++nw)
+    {
+        amrex::Print() << "nwork = " << nw << std::endl;
+    
+        // Time w/o mask
+        {
+            t_all = amrex::second();
+            BL_PROFILE("** Full");
+    
+            for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+            {
+                const Box& bx = mfi.validbox();
+                auto arr = mf.array(mfi); 
+    
+                amrex::ParallelFor(bx, ncomp,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+                {
+                    compute_bound(i, j, k, c, arr, nw);
+                });        
+            }
+            t_all = amrex::second() - t_all;
+        }
+    
+        mf.setVal(start_val);
+    
+        // Time of interior
+        {
+            t_int = amrex::second();
+            BL_PROFILE("** Interior");
+    
+            for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+            {
+                const Box& bx = mfi.validbox();
+                auto arr = mf.array(mfi); 
+                auto mask = bmask.const_array(mfi);
+    
+                amrex::ParallelFor(bx, ncomp,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+                {
+                    if (!mask(i,j,k)) {
+                        compute_bound(i, j, k, c, arr, nw);
+                    }
+                });        
+            }
+            t_int = amrex::second() - t_int;
+        }
+    
+        mf.setVal(start_val);
+    
+        // Time of boundary
+        {
+            t_bound = amrex::second();
+            BL_PROFILE("** Boundary");
+    
+            for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+            {
+                const Box& bx = mfi.validbox();
+                auto arr = mf.array(mfi); 
+                auto mask = bmask.const_array(mfi);
+    
+                amrex::ParallelFor(bx, ncomp,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+                {
+                    if (mask(i,j,k)) {
+                        compute_bound(i, j, k, c, arr, nw);
+                    }
+                });        
+            }
+            t_bound = amrex::second() - t_bound;
+        }
+    
+        amrex::Print() << "All: " << t_all << std::endl;
+        amrex::Print() << "Interior: " << t_int << std::endl;
+        amrex::Print() << "Boundary: " << t_bound << std::endl;
+        amrex::Print() << "% Interior: " << 100*(t_int/t_all) << std::endl;
+        amrex::Print() << "% Boundary: " << 100*(t_bound/t_all) << std::endl;
+        amrex::Print() << std::endl;
+    }
+
+// =======================================================
+
+    double t_fb, t_comb;
+
+    // Separate FillBoundary 
+    {
+        t_fb = amrex::second();
+
+        BL_PROFILE("Separate FB");
+        mf.FillBoundary(nghost);
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+        {
+            const Box& bx = mfi.validbox();
+            auto arr = mf.array(mfi);
+
+            amrex::ParallelFor(bx, ncomp,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+            {
+                compute_bound(i, j, k, c, arr, ntest);
+            });
+        }
+
+        t_fb = amrex::second() - t_fb;
+    } 
+
+    // Combine FillBoundary & Pieces
+    {
+        t_comb = amrex::second();
+        BL_PROFILE("Combine FB");
+
+        mf.FillBoundary_nowait(nghost);
+
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+        {
+            const Box& bx = mfi.validbox();
+            auto arr = mf.array(mfi);
+            auto mask = bmask.const_array(mfi);
+
+            amrex::ParallelFor(bx, ncomp,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+            {
+                if (!mask(i,j,k)) {
+                    compute_bound(i, j, k, c, arr, ntest);
+                }
+            });
+        }
+
+        mf.FillBoundary_finish();
+
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+        {
+            const Box& bx = mfi.validbox();
+            auto arr = mf.array(mfi);
+            auto mask = bmask.const_array(mfi);
+
+            amrex::ParallelFor(bx, ncomp,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, int c)
+            {
+                if (mask(i,j,k)) {
+                    compute_bound(i, j, k, c, arr, ntest);
+                }
+            });
+        }
+        t_comb = amrex::second() - t_comb;
+    }
+
+    amrex::Print() << "FB then Work: " << t_fb << std::endl;
+    amrex::Print() << "FB w/ Overlap: " << t_comb << std::endl;
+    amrex::Print() << "% Improvement: " << 100*(t_fb-t_comb)/(t_fb) << std::endl;
+}
